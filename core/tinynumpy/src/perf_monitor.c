7a4b8dc8352821c99f2d74cad19da98b714e9dce
tian <xiatian1996cd@gmail.com>
2026-02-16 17:37:12 +0800

    feat: implement basic perf_event wrapper for HW cache counters

---
+++ core/tinynumpy/src/perf_monitor.c
@@ -0,0 +1,54 @@
+#define _GNU_SOURCE
+#include "perf_monitor.h"
+#include <asm/unistd.h>
+#include <linux/perf_event.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+int setup_perf_event(int event) {
+  struct perf_event_attr pe = {.type = PERF_TYPE_HW_CACHE,
+                               .size = sizeof(struct perf_event_attr),
+                               .config = event,
+                               .disabled = 1,
+                               .exclude_kernel = 1,
+                               .exclude_hv = 1};
+
+  int fd = syscall(__NR_perf_event_open, &pe, 0, -1, -1, 0);
+  if (fd < 0) {
+    perror("Error: perf_event_open failed.");
+  }
+
+  return fd;
+}
+
+void start_perf_event(int fd) {
+  if (fd < 0) {
+    fprintf(stderr, "Error: Cannot stop. Invalid file descriptor (fd=%d).\n",
+            fd);
+  }
+  ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+  ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+}
+
+void stop_perf_event(int fd) {
+  if (fd < 0) {
+    fprintf(stderr, "Error: Cannot stop. Invalid file descriptor (fd=%d).\n",
+            fd);
+  }
+  ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+}
+
+long long read_perf_event(int fd) {
+  if (fd < 0) {
+    fprintf(stderr, "Error: Cannot read. Invalid file descriptor (fd=%d).\n",
+            fd);
+  }
+
+  long long count;
+  if (read(fd, &count, sizeof(long long)) != sizeof(long long)) {
+    perror("Error: Failed to read perf event value.");
+    return -1;
+  }
+  return count;
+}
